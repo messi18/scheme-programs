9cfbcab9d50d60c535786efa0921b817ffeeefea
lance <messi_18@live.cn>
2012-06-14 00:03:37 +0800

    correct some mistakes

--- p111.scm
+++ p111.scm
@@ -1,15 +1,15 @@
 ;;this is for the practice 1.11 from SICP
-(define (f n) (
-  (define (f-iter a b c) (
+(define (f n) 
+  (define (f-iter a b c) 
     (cond
       ((= n 2) (if (> a n) a 2))
       ((< n 2) n)
       (else (f-iter (+ a (* 2 b) (* 3 c)) a b (- n 1)))
     )
-  ))
+  )
 
   (f-iter 2 1 0 n)
-  ))
+)
 
 ;; test
 (display (= (f 0) 0))
